4b7b63e5a0e558c669c3b38bb68fa0c65d40f540
SonSuBin129 <ttokky0129@gmail.com>
2024-07-25 02:00:08 +0900

    chore: alias 세팅 + shadcn 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import generouted from "@generouted/react-router/plugin";
@@ -5,4 +7,9 @@ import generouted from "@generouted/react-router/plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), generouted()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
